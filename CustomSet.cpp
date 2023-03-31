9e515f1a5904b027d15b9652b58058ec08d9c074
the80hz <dnl.vlgv@mail.ru>
2023-03-31 13:00:56 +0400

    average time of filling

---
+++ CustomSet.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Даниил Вольгов on 31.03.2023.
+//
+
+#include "CustomSet.h"
--- main.cpp
+++ main.cpp
@@ -1,192 +1,64 @@
 #include <iostream>
-#include "set.h"
+#include <chrono>
+#include "CustomSet.h"
 
-// generator
-size_t lcg(){
+size_t lcg() {
     static size_t x = 0;
-    x = (1021*x+24631) % 116640;
+    x = (1021 * x + 24631) % 116640;
     return x;
 }
 
-class CustomSet {
-public:
-    CustomSet() : root(nullptr) {}
+void testContainer(int containerSize, int fillAttempts, int searchAttempts, int addRemoveAttempts) {
+    using namespace std::chrono;
 
-    CustomSet(const CustomSet& other) : root(nullptr) {
-        copyTree(root, other.root);
-    }
+    CustomSet container;
+    int64_t totalTimeFill = 0;
+    int64_t totalTimeSearch = 0;
+    int64_t totalTimeAddRemove = 0;
 
-    ~CustomSet() {
-        clear(root);
-    }
-
-    CustomSet& operator=(const CustomSet& other) {
-        if (this != &other) {
-            clear(root);
-            copyTree(root, other.root);
+    for (int i = 0; i < fillAttempts; ++i) {
+        CustomSet tmpContainer;
+        auto startFill = steady_clock::now();
+        for (int j = 0; j < containerSize; ++j) {
+            tmpContainer.insert(lcg());
         }
-        return *this;
+        auto endFill = steady_clock::now();
+        totalTimeFill += duration_cast<microseconds>(endFill - startFill).count();
+        container = tmpContainer;
     }
 
-    void print() const {
-        printInOrder(root);
-        std::cout << std::endl;
+    for (int i = 0; i < searchAttempts; ++i) {
+        auto startSearch = steady_clock::now();
+        container.contains(lcg());
+        auto endSearch = steady_clock::now();
+        totalTimeSearch += duration_cast<microseconds>(endSearch - startSearch).count();
     }
 
-    bool insert(int key) {
-        return insert(root, key);
+    for (int i = 0; i < addRemoveAttempts; ++i) {
+        int randomNum = lcg();
+
+        auto startAddRemove = steady_clock::now();
+        container.insert(randomNum);
+        container.erase(randomNum);
+        auto endAddRemove = steady_clock::now();
+        totalTimeAddRemove += duration_cast<microseconds>(endAddRemove - startAddRemove).count();
     }
 
-    [[nodiscard]] bool contains(int key) const {
-        return contains(root, key);
-    }
-
-    bool erase(int key) {
-        return erase(root, key);
-    }
-
-private:
-    struct Node {
-        int key;
-        Node* left;
-        Node* right;
-
-        explicit Node(int k) : key(k), left(nullptr), right(nullptr) {}
-    };
-
-    Node* root;
-
-    void clear(Node* node) {
-        if (node) {
-            clear(node->left);
-            clear(node->right);
-            delete node;
-        }
-    }
-
-    void copyTree(Node*& dest, Node* src) {
-        if (src) {
-            dest = new Node(src->key);
-            copyTree(dest->left, src->left);
-            copyTree(dest->right, src->right);
-        }
-    }
-
-    void printInOrder(Node* node) const {
-        if (node) {
-            printInOrder(node->left);
-            std::cout << node->key << " ";
-            printInOrder(node->right);
-        }
-    }
-
-    bool insert(Node*& node, int key) {
-        if (!node) {
-            node = new Node(key);
-            return true;
-        }
-
-        if (key < node->key) {
-            return insert(node->left, key);
-        } else if (key > node->key) {
-            return insert(node->right, key);
-        } else {
-            return false; // Element already exists
-        }
-    }
-
-    bool contains(Node* node, int key) const {
-        if (!node) {
-            return false;
-        }
-
-        if (key < node->key) {
-            return contains(node->left, key);
-        } else if (key > node->key) {
-            return contains(node->right, key);
-        } else {
-            return true; // Element found
-        }
-    }
-
-    static Node* findMin(Node* node) {
-        while (node->left) {
-            node = node->left;
-        }
-        return node;
-    }
-
-    bool erase(Node*& node, int key) {
-        if (!node) {
-            return false; // Element not found
-        }
-
-        if (key < node->key) {
-            return erase(node->left, key);
-        } else if (key > node->key) {
-            return erase(node->right, key);
-        } else {
-            if (!node->left && !node->right) {
-                delete node;
-                node = nullptr;
-            } else if (!node->left) {
-                Node* temp = node;
-                node = node->right;
-                delete temp;
-            } else if (!node->right) {
-                Node* temp = node;
-                node = node->left;
-                delete temp;
-            } else {
-                node->key = findMin(node->right)->key;
-                return erase(node->right, node->key);
-            }
-            return true; // Element deleted
-        }
-    }
-};
-
+    std::cout << "Container size: " << containerSize << std::endl;
+    std::cout << "Average fill time: " << (totalTimeFill / static_cast<double>(fillAttempts)) << " microseconds" << std::endl;
+    std::cout << "Average search time: " << (totalTimeSearch / static_cast<double>(searchAttempts)) << " microseconds" << std::endl;
+    std::cout << "Average add/remove time: " << (totalTimeAddRemove / static_cast<double>(addRemoveAttempts)) << " microseconds" << std::endl;
+    std::cout << std::endl;
+}
 
 int main() {
-    CustomSet mySet;
+    int fillAttempts = 100;
+    int searchAttempts = 1000;
+    int addRemoveAttempts = 1000;
 
-    // Insert elements
-    mySet.insert(5);
-    mySet.insert(3);
-    mySet.insert(8);
-    mySet.insert(1);
-    mySet.insert(4);
-
-    // Print the set
-    std::cout << "My set: ";
-    mySet.print();
-
-    // Check for presence of elements
-    std::cout << "Contains 3: " << mySet.contains(3) << std::endl;
-    std::cout << "Contains 7: " << mySet.contains(7) << std::endl;
-
-    // Delete elements
-    mySet.erase(3);
-    mySet.erase(8);
-
-    // Print the set after deleting elements
-    std::cout << "My set after deletions: ";
-    mySet.print();
-
-    // Create a copy of the set using the copy constructor
-    CustomSet copiedSet(mySet);
-
-    // Print the copied set
-    std::cout << "Copied set: ";
-    copiedSet.print();
-
-    // Create a new set and assign the original set using the assignment operator
-    CustomSet assignedSet;
-    assignedSet = mySet;
-
-    // Print the assigned set
-    std::cout << "Assigned set: ";
-    assignedSet.print();
+    testContainer(1000, fillAttempts, searchAttempts, addRemoveAttempts);
+    testContainer(10000, fillAttempts, searchAttempts, addRemoveAttempts);
+    testContainer(100000, fillAttempts, searchAttempts, addRemoveAttempts);
 
     return 0;
 }
