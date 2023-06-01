ecba3ddcb882835890a47cf835610228a3714f69
the80hz <dnl.vlgv@mail.ru>
2023-06-01 16:27:18 +0400

    Revert "wtf"
    This reverts commit cb9efe911f199ad4c39b9715ffb7fffe02a9a51c.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
 #include <chrono>
-#include <vector>
-#include <algorithm>
 #include "CustomSet.h"
 
 size_t lcg() {
@@ -10,29 +8,19 @@ size_t lcg() {
     return x;
 }
 
-template<typename Container>
-void testContainer(const std::string& containerName,
-                   int containerSize,
-                   int fillAttempts,
-                   int searchAttempts,
-                   int addRemoveAttempts,
-                   std::function<void(Container&, int)> insertOp,
-                   std::function<bool(Container&, int)> findOp) {
+void testContainer(int containerSize, int fillAttempts, int searchAttempts, int addRemoveAttempts) {
     using namespace std::chrono;
 
-    Container container;
+    CustomSet container;
     int64_t totalTimeFill = 0;
     int64_t totalTimeSearch = 0;
     int64_t totalTimeAddRemove = 0;
 
     for (int i = 0; i < fillAttempts; ++i) {
-        Container tmpContainer;
+        CustomSet tmpContainer;
         auto startFill = steady_clock::now();
         for (int j = 0; j < containerSize; ++j) {
-            int randomNum = lcg();
-            if (!findOp(tmpContainer, randomNum)) {
-                insertOp(tmpContainer, randomNum);
-            }
+            tmpContainer.insert(lcg());
         }
         auto endFill = steady_clock::now();
         totalTimeFill += duration_cast<microseconds>(endFill - startFill).count();
@@ -41,31 +29,22 @@ void testContainer(const std::string& containerName,
 
     for (int i = 0; i < searchAttempts; ++i) {
         auto startSearch = steady_clock::now();
-        findOp(container, lcg());
+        container.contains(lcg());
         auto endSearch = steady_clock::now();
         totalTimeSearch += duration_cast<microseconds>(endSearch - startSearch).count();
     }
-
 
     for (int i = 0; i < addRemoveAttempts; ++i) {
         int randomNum = lcg();
 
         auto startAddRemove = steady_clock::now();
-        bool found = findOp(container, randomNum);
-        if (!found) {
-            insertOp(container, randomNum);
-        } else {
-            // Remove operation not required for CustomSet, only for std::vector
-            if constexpr (std::is_same_v<Container, std::vector<int>>) {
-                container.erase(std::remove(container.begin(), container.end(), randomNum), container.end());
-            }
-        }
+        container.insert(randomNum);
+        container.erase(randomNum);
         auto endAddRemove = steady_clock::now();
         totalTimeAddRemove += duration_cast<microseconds>(endAddRemove - startAddRemove).count();
     }
 
-
-    std::cout << containerName << " size: " << containerSize << std::endl;
+    std::cout << "Container size: " << containerSize << std::endl;
     std::cout << "Average fill time: " << (totalTimeFill / static_cast<double>(fillAttempts)) << " microseconds" << std::endl;
     std::cout << "Average search time: " << (totalTimeSearch / static_cast<double>(searchAttempts)) << " microseconds" << std::endl;
     std::cout << "Average add/remove time: " << (totalTimeAddRemove / static_cast<double>(addRemoveAttempts)) << " microseconds" << std::endl;
@@ -77,28 +56,9 @@ int main() {
     int searchAttempts = 1000;
     int addRemoveAttempts = 1000;
 
-    testContainer<CustomSet>("CustomSet", 1000, fillAttempts, searchAttempts, addRemoveAttempts,
-                             [](CustomSet& container, int value) { container.insert(value); },
-                             [](CustomSet& container, int value) { return container.contains(value); });
-
-    testContainer<std::vector<int>>("std::vector<int>", 1000, fillAttempts, searchAttempts, addRemoveAttempts,
-                                    [](std::vector<int>& container, int value) { container.insert(container.end(), value); },
-                                    [](std::vector<int>& container, int value) { return std::find(container.begin(), container.end(), value) != container.end(); });
-    testContainer<CustomSet>("CustomSet", 10000, fillAttempts, searchAttempts, addRemoveAttempts,
-                             [](CustomSet& container, int value) { container.insert(value); },
-                             [](CustomSet& container, int value) { return container.contains(value); });
-
-    testContainer<std::vector<int>>("std::vector<int>", 10000, fillAttempts, searchAttempts, addRemoveAttempts,
-                                    [](std::vector<int>& container, int value) { container.insert(container.end(), value); },
-                                    [](std::vector<int>& container, int value) { return std::find(container.begin(), container.end(), value) != container.end(); });
-    testContainer<CustomSet>("CustomSet", 100000, fillAttempts, searchAttempts, addRemoveAttempts,
-                             [](CustomSet& container, int value) { container.insert(value); },
-                             [](CustomSet& container, int value) { return container.contains(value); });
-
-    testContainer<std::vector<int>>("std::vector<int>", 100000, fillAttempts, searchAttempts, addRemoveAttempts,
-                                    [](std::vector<int>& container, int value) { container.insert(container.end(), value); },
-                                    [](std::vector<int>& container, int value) { return std::find(container.begin(), container.end(), value) != container.end(); });
-
+    testContainer(1000, fillAttempts, searchAttempts, addRemoveAttempts);
+    testContainer(10000, fillAttempts, searchAttempts, addRemoveAttempts);
+    testContainer(100000, fillAttempts, searchAttempts, addRemoveAttempts);
 
     return 0;
 }
