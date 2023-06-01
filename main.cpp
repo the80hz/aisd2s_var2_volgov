0f7bc0960d91ccb9bd678e064d6e7bcb2b1db844
the80hz <dnl.vlgv@mail.ru>
2023-06-01 16:27:19 +0400

    Revert "base"
    This reverts commit 994df1bc33895e203aee7cfa040d0aaffde8222a.

--- main.cpp
+++ main.cpp
@@ -1,13 +1,4 @@
 #include <iostream>
-#include "set.h"
-
-// generator
-size_t lcg(){
-    static size_t x = 0;
-    x = (1021*x+24631) % 116640;
-    return x;
-}
-
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
