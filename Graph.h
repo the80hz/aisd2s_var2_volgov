6d2e18882e888d7bc0fd7bd84098af19b48ce530
the80hz <dnl.vlgv@mail.ru>
2023-06-07 13:24:51 +0400

    работает меню, задача реализовата не полностью

--- Graph.h
+++ Graph.h
@@ -5,13 +5,11 @@
 #ifndef AISD2S_VAR2_VOLGOV_GRAPH_H
 #define AISD2S_VAR2_VOLGOV_GRAPH_H
 
-#include <iostream>
 #include <vector>
 #include <unordered_map>
 #include <queue>
 #include <limits>
 
-// Реализовать шаблонный класс, моделирющий ориентированный граф
 template<typename Vertex, typename Distance = double>
 class Graph {
 public:
@@ -21,32 +19,201 @@ public:
         Distance distance;
     };
 
-    bool has_vertex(const Vertex& v) const;
-    void add_vertex(const Vertex& v);
-    bool remove_vertex(const Vertex& v);
-    std::vector<Vertex> vertices() const;
+    bool has_vertex(const Vertex& v) const {
+        return adjacency_list.count(v) > 0;
+    }
 
-    void add_edge(const Vertex& from, const Vertex& to, const Distance& d);
-    bool remove_edge(const Vertex& from, const Vertex& to);
-    bool remove_edge(const Edge& e);
-    bool has_edge(const Vertex& from, const Vertex& to) const;
-    bool has_edge(const Edge& e);
+    void add_vertex(const Vertex& v) {
+        if (!has_vertex(v)) {
+            adjacency_list[v] = std::vector<Edge>();
+        }
+    }
 
-    std::vector<Edge> edges(const Vertex& vertex);
+    bool remove_vertex(const Vertex& v) {
+        if (has_vertex(v)) {
+            adjacency_list.erase(v);
 
-    size_t order() const;
-    size_t degree() const;
+            for (auto& pair : adjacency_list) {
+                auto& edges = pair.second;
+                edges.erase(std::remove_if(edges.begin(), edges.end(), [&](const Edge& edge) {
+                    return edge.from == v || edge.to == v;
+                }), edges.end());
+            }
 
-    std::vector<Edge> shortest_path(const Vertex& from, const Vertex& to) const;
-    std::vector<Vertex> walk(const Vertex& start_vertex) const;
+            return true;
+        }
+
+        return false;
+    }
+
+    std::vector<Vertex> vertices() const {
+        std::vector<Vertex> result;
+        result.reserve(adjacency_list.size());
+
+        for (const auto& pair : adjacency_list) {
+            result.push_back(pair.first);
+        }
+
+        return result;
+    }
+
+    void add_edge(const Vertex& from, const Vertex& to, const Distance& d) {
+        add_vertex(from);
+        add_vertex(to);
+
+        adjacency_list[from].push_back({from, to, d});
+    }
+
+    bool remove_edge(const Vertex& from, const Vertex& to) {
+        if (has_vertex(from) && has_vertex(to)) {
+            auto& edges = adjacency_list[from];
+            auto it = std::find_if(edges.begin(), edges.end(), [&](const Edge& edge) {
+                return edge.to == to;
+            });
+
+            if (it != edges.end()) {
+                edges.erase(it);
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    bool remove_edge(const Edge& e) {
+        return remove_edge(e.from, e.to);
+    }
+
+    bool has_edge(const Vertex& from, const Vertex& to, const Distance& distance) const {
+        if (has_vertex(from) && has_vertex(to)) {
+            const auto& edges = adjacency_list.at(from);
+            return std::any_of(edges.begin(), edges.end(), [&](const Edge& edge) {
+                return edge.to == to && edge.distance == distance;
+            });
+        }
+
+        return false;
+    }
+
+    bool has_edge(const Edge& e) {
+        return has_edge(e.from, e.to, e.distance);
+    }
+
+
+    std::vector<Edge> edges(const Vertex& vertex) {
+        std::vector<Edge> result;
+
+        if (has_vertex(vertex)) {
+            result = adjacency_list[vertex];
+        }
+
+        return result;
+    }
+
+    [[nodiscard]] size_t order() const {
+        return adjacency_list.size();
+    }
+
+    [[nodiscard]] size_t degree() const {
+        size_t max_degree = 0;
+
+        for (const auto& pair : adjacency_list) {
+            max_degree = std::max(max_degree, pair.second.size());
+        }
+
+        return max_degree;
+    }
+
+    std::vector<Edge> shortest_path(const Vertex& from, const Vertex& to) const {
+        std::unordered_map<Vertex, Distance> distances;
+        std::unordered_map<Vertex, Vertex> parents;
+
+        for (const auto& pair : adjacency_list) {
+            distances[pair.first] = std::numeric_limits<Distance>::max();
+        }
+
+        distances[from] = Distance();
+
+        for (size_t i = 0; i < adjacency_list.size() - 1; ++i) {
+            for (const auto& pair : adjacency_list) {
+                const auto& vertex = pair.first;
+                const auto& edges = pair.second;
+
+                for (const auto& edge : edges) {
+                    const auto& from_vertex = edge.from;
+                    const auto& to_vertex = edge.to;
+                    const auto& distance = edge.distance;
+
+                    if (distances[from_vertex] + distance < distances[to_vertex]) {
+                        distances[to_vertex] = distances[from_vertex] + distance;
+                        parents[to_vertex] = from_vertex;
+                    }
+                }
+            }
+        }
+
+        std::vector<Edge> path;
+
+        if (parents.count(to) > 0) {
+            Vertex current = to;
+
+            while (current != from) {
+                const Vertex parent = parents[current];
+                const Distance distance = distances[current] - distances[parent];
+
+                path.push_back({parent, current, distance});
+
+                current = parent;
+            }
+
+            std::reverse(path.begin(), path.end());
+        }
+
+        return path;
+    }
+
+    void walk(const Vertex& start_vertex, std::function<void(const Vertex&)> action) const {
+        std::unordered_map<Vertex, bool> visited;
+        walkHelper(start_vertex, visited, action);
+    }
+
+    const std::vector<Vertex>& vertices(std::vector<Vertex>& result) const {
+        result.clear();
+        result.reserve(adjacency_list.size());
+
+        for (const auto& pair : adjacency_list) {
+            result.push_back(pair.first);
+        }
+
+        return result;
+    }
+
+    const std::vector<Edge>& edges(const Vertex& vertex, std::vector<Edge>& result) const {
+        result.clear();
+
+        if (has_vertex(vertex)) {
+            result = adjacency_list[vertex];
+        }
+
+        return result;
+    }
 
 private:
     std::unordered_map<Vertex, std::vector<Edge>> adjacency_list;
 
-    void walkHelper(const Vertex& current, std::unordered_map<Vertex, bool>& visited, std::vector<Vertex>& result) const;
-};
+    void walkHelper(const Vertex& current, std::unordered_map<Vertex, bool>& visited, std::function<void(const Vertex&)> action) const {
+        visited[current] = true;
+        action(current);
 
-// Определение методов
+        if (adjacency_list.count(current) > 0) {
+            for (const auto& edge : adjacency_list.at(current)) {
+                if (!visited[edge.to]) {
+                    walkHelper(edge.to, visited, action);
+                }
+            }
+        }
+    }
+};
 
 
 #endif //AISD2S_VAR2_VOLGOV_GRAPH_H
--- main.cpp
+++ main.cpp
@@ -1,21 +1,147 @@
-/*  В рамках лабораторной работы разрешено использование STL-контейнеров,
- *  а также классов и функций из заголовочных файлов <memory>, <iterator>, <functional>, <algorithm> и <numeric>.
- *  Запрещено использование функций ручного управления памятью (malloc, free, new и delete).
- *  Работу с памятью делегировать STL-контейнерам и «умным указателям» std::uniqie_ptr/std::shared_ptr.
- *
- *  Нужно написать:
- *  1) Класс Graph
- *  2) Демонстрационная программа:
- *      Пусть дан связный граф, в котором узлы – это торговые точки.
- *      Необходимо превратить одну из торговых точек в склад.
- *      Цена доставки от склада в точку зависит от расстояния.
- *      Найдите оптимальную с точки зрения максимальных затрат точку (т.е. точку,
- *      для которой максимальное расстояние до любой другой точки минимально).
- */
-
 #include <iostream>
+#include <vector>
+#include <functional>
+#include "Graph.h"
+
+// Функция для печати вершин, встреченных при обходе
+template<typename Vertex>
+void printVertices(const std::vector<Vertex>& vertices) {
+    std::cout << "Visited vertices: ";
+    for (const auto& vertex : vertices) {
+        std::cout << vertex << " ";
+    }
+    std::cout << std::endl;
+}
+
+// Функция для поиска оптимальной точки склада
+template<typename Vertex, typename Distance>
+Vertex findOptimalWarehouse(const Graph<Vertex, Distance>& graph) {
+    Vertex optimalWarehouse;
+    Distance minMaxDistance = std::numeric_limits<Distance>::max();
+
+    for (const auto& vertex : graph.vertices()) {
+        Distance maxDistance = 0;
+        auto shortestPaths = graph.shortest_path(vertex, vertex);
+
+        for (const auto& path : shortestPaths) {
+            if (path.distance > maxDistance) {
+                maxDistance = path.distance;
+            }
+        }
+
+        if (maxDistance < minMaxDistance) {
+            minMaxDistance = maxDistance;
+            optimalWarehouse = vertex;
+        }
+    }
+
+    return optimalWarehouse;
+}
+
+// Функция для печати меню и обработки выбора пользователя
+void printMenu() {
+    std::cout << "-------- Menu --------" << std::endl;
+    std::cout << "1. Add vertex" << std::endl;
+    std::cout << "2. Add edge" << std::endl;
+    std::cout << "3. Print all vertices" << std::endl;
+    std::cout << "4. Print all edges of a vertex" << std::endl;
+    std::cout << "5. Perform graph traversal" << std::endl;
+    std::cout << "6. Find shortest path between vertices" << std::endl;
+    std::cout << "7. Find optimal warehouse" << std::endl;
+    std::cout << "8. Exit" << std::endl;
+    std::cout << "-----------------------" << std::endl;
+    std::cout << "Enter your choice: ";
+}
 
 int main() {
+    Graph<int, double> graph;
+
+    int choice;
+    do {
+        printMenu();
+        std::cin >> choice;
+
+        switch (choice) {
+            case 1: {
+                int vertex;
+                std::cout << "Enter vertex value: ";
+                std::cin >> vertex;
+                graph.add_vertex(vertex);
+                break;
+            }
+            case 2: {
+                int from, to;
+                double distance;
+                std::cout << "Enter source vertex: ";
+                std::cin >> from;
+                std::cout << "Enter destination vertex: ";
+                std::cin >> to;
+                std::cout << "Enter distance: ";
+                std::cin >> distance;
+                graph.add_edge(from, to, distance);
+                break;
+            }
+            case 3: {
+                std::vector<int> vertices = graph.vertices();
+                std::cout << "Vertices: ";
+                for (const auto& vertex : vertices) {
+                    std::cout << vertex << " ";
+                }
+                std::cout << std::endl;
+                break;
+            }
+            case 4: {
+                int vertex;
+                std::cout << "Enter vertex: ";
+                std::cin >> vertex;
+                std::vector<typename Graph<int, double>::Edge> edges = graph.edges(vertex);
+                std::cout << "Edges of vertex " << vertex << ": ";
+                for (const auto& edge : edges) {
+                    std::cout << "(" << edge.from << "->" << edge.to << ") ";
+                }
+                std::cout << std::endl;
+                break;
+            }
+            case 5: {
+                int startVertex;
+                std::cout << "Enter start vertex: ";
+                std::cin >> startVertex;
+                std::vector<int> visitedVertices;
+                graph.walk(startVertex, [&](const int& vertex) {
+                    visitedVertices.push_back(vertex);
+                });
+                printVertices(visitedVertices);
+                break;
+            }
+            case 6: {
+                int from, to;
+                std::cout << "Enter source vertex: ";
+                std::cin >> from;
+                std::cout << "Enter destination vertex: ";
+                std::cin >> to;
+                std::vector<typename Graph<int, double>::Edge> shortestPath = graph.shortest_path(from, to);
+                std::cout << "Shortest path from " << from << " to " << to << ": ";
+                for (const auto& edge : shortestPath) {
+                    std::cout << edge.from << "->" << edge.to << " ";
+                }
+                std::cout << std::endl;
+                break;
+            }
+            case 7: {
+                int optimalWarehouse = findOptimalWarehouse(graph);
+                std::cout << "Optimal warehouse: " << optimalWarehouse << std::endl;
+                break;
+            }
+            case 8: {
+                std::cout << "Exiting the program..." << std::endl;
+                break;
+            }
+            default:
+                std::cout << "Invalid choice. Please try again." << std::endl;
+        }
+        std::cout << std::endl;
+
+    } while (choice != 8);
 
     return 0;
 }
